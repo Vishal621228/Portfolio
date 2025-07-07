8068384b89779623b05fd7cee0dbc7aefdeb9ac8
Vishal Prajapat <vishal.hit555@gmail.com>
2025-07-07 12:02:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// I am still learning JavaScript
+// and when i am done i am going to add some new things to my website like -
+// copy email and number when user click on it
+// reload my progress bars when thry come into viewport
+// make a data base for form
+// if someone watching have any suggestions please leave me a message on linked or twitter
+// Thank you for your time if you read so far... 
